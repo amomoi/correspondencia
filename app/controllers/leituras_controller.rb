1c78e77dd0801be447b67b77301960d13bac0123
amomoi <momoi.alexandre@gmail.com>
2022-08-05 15:42:18 -0300

    Correcao path check_ativo

--- app/controllers/leituras_controller.rb
+++ app/controllers/leituras_controller.rb
@@ -76,7 +76,7 @@ class LeiturasController < ApplicationController
 
   def checkativo
     releaseCrossDomain
-    @leitura = Leitura.where(torre: params[:torre])
+    @leitura = Leitura.where(torre: params[:torre], n_apto: params[:n_apto], ativo_inativo[:ativo_inativo])
     render json: @leitura
   end
 

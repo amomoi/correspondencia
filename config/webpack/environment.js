993f464c10536b50c3b63aed9da24c28c76b89b5
Alexandre Momoi <momoi.alexandre@gmail.com>
2024-08-25 12:36:16 -0300

    p.json v19

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,10 +1,12 @@
 
 const { environment } = require('@rails/webpacker');
 
+module.exports = environment;
+
 module.exports = {
     node: {
       __dirname: true,
       __filename: true,
-      global: true,
+      global: true
     },
   };
